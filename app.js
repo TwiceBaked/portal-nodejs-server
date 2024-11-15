653e28f95947f43971f4ca85d62a18f79017365c
TwiceBaked <gabrieljcarlson@lewisu.edu>
2024-11-15 14:26:30 -0600

    updated the packages, added a git ignore

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('Hello, Azure!');
 });
 
+app.get('/hello', (req, res) => {
+  res.send('Hello, Azure! This static react site and the Nodejs server this message is being pulled from were created using the Azure portal');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
